035cdaf75a753d5d912ea45af0934ffe6f46a157
faiqoh04 <sifaafaiqoh@gmail.com>
2024-03-08 19:32:50 +0700

    Pertemuan 4 - PHP

--- Jobsheet 4/tipe_data.php
+++ Jobsheet 4/tipe_data.php
@@ -13,4 +13,37 @@ echo "variabel e: {$e} <br>";
 
 var_dump($e);
 
+$nilaiMatematika = 5.1;
+$nilaiIPA = 6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) / 3;
+
+echo "matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "bahasa indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "rata-rata: {$rataRata} <br>";
+var_dump($rataRata);
+
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
+
+$namaDepan = "Ibnu";
+$namaBelakang = 'Jakaria';
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan . ' ' . $namaBelakang;
+
+echo "Nama Depan: {$namaDepan} <br>";
+echo 'Nama Belakang: ' . $namaBelakang . '<br>';
+
+echo $namaLengkap;
+
+$listMahasiswa = ["Wahid Abdullah", "Elmo Bachtiar", "Lendis Fabri"];
+echo $listMahasiswa[0];
+
 ?>
